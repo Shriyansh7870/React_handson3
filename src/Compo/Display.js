04d01d21fcd31a2645d74bdd95dc4ba7d40e7b8f
Shriyansh7870 <Shriyansh7870@gmail.com>
2023-09-03 00:51:15 +0530

    first commit

---
+++ src/Compo/Display.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+
+class DataStore extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="parent">
+        {this.props.value.map((element) => (
+          <div key={element.id} className="child">
+            <p className="para">
+              Name of Student: {element.name} || Department:{" "}
+              {element.department} || Rating: {element.rating}
+            </p>
+          </div>
+        ))}
+        <button onClick={this.props.value1} className="go">
+          Go Back
+        </button>
+      </div>
+    );
+  }
+}
+
+export default DataStore;
